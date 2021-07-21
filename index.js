cd14b73b91b6530f7effd3da03afc6ef6f26eac9
Connor Cowan <ccowan003@comcast.net>
2021-07-21 08:51:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'some value';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    //return customerName.toUpperCase(); They don't want this. Want poor code.
+    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';  //declare global variable in function scope
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Chucky'
+}
